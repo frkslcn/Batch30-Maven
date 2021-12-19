b61917898bdeb9fc1a73bdd4d1f378fc08b2de7a
Bestami Tosun <frkslcn31@gmail.com>
2021-12-19 08:44:48 +0100

    4.gun sonu

---
+++ src/test/java/day05/C01_Notasyon.java
@@ -0,0 +1,14 @@
+package day05;
+
+import org.junit.Test;
+
+public class C01_Notasyon {
+    //selenyumdada metodlar olusturabiliriz,cagrilmadikca calismazalr
+public void ilkTestMetodu(){
+    System.out.println("Ilk test metodumuz");
+}
+@Test
+public void notasyonluIlkTestMetodu(){
+    System.out.println("notasyonlu ilk test methodumuz");
+}
+}
